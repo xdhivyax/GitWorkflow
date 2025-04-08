c82f1f8b8da0d886c1c6b97191aa302c13d06e5c
Dhivya <113448671+xdhivyax@users.noreply.github.com>
2025-04-08 17:23:01 +0530

    Update worker_node_creation.tf

--- worker_node_creation.tf
+++ worker_node_creation.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "ACCESS_KEY"
-  secret_key = "SECRET_KEY"  
+  access_key = "AWS_ACCESS_KEY_ID"
+  secret_key = "AWS_SECRET_ACCESS_KEY"  
   region = "ap-south-1"  
 }
 resource "aws_instance" "worker" {
